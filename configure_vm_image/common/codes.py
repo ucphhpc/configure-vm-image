15ecaa02beaffcf55a9579a2e7928048dfdbfd91
Rasmus Munk <rasmunk@users.noreply.github.com>
2024-12-16 09:51:45 +0100

    Add missing common codes

--- configure_vm_image/common/codes.py
+++ configure_vm_image/common/codes.py
@@ -5,17 +5,22 @@ PATH_LOAD_ERROR = 2
 PATH_LOAD_ERROR_MSG = "Failed to load path: {}"
 PATH_CREATE_ERROR = 3
 PATH_CREATE_ERROR_MSG = "Failed to create path: {} - error: {}"
-MISSING_ATTRIBUTE_ERROR = 4
+CONFIGURE_IMAGE_ERROR = 4
+CONFIGURE_IMAGE_ERROR_MSG = "Failed to configure image: {} - error: {}"
+RESET_IMAGE_ERROR = 5
+RESET_IMAGE_ERROR_MSG = "Failed to reset image: {} - error: {}"
+
+MISSING_ATTRIBUTE_ERROR = 6
 MISSING_ATTRIBUTE_ERROR_MSG = "Missing attribute: {} in {}"
-INVALID_ATTRIBUTE_TYPE_ERROR = 5
+INVALID_ATTRIBUTE_TYPE_ERROR = 7
 INVALID_ATTRIBUTE_TYPE_ERROR_MSG = (
     "Invalid attribute type: {} for value: {} - must be {}"
 )
-CHECKSUM_ERROR = 6
-RESIZE_ERROR = 7
+CHECKSUM_ERROR = 8
+RESIZE_ERROR = 9
 RESIZE_ERROR_MSG = "Failed to resize path: {}"
-CHECK_ERROR = 8
+CHECK_ERROR = 10
 CHECK_ERROR_MSG = "Failed to check path: {}"
-JSON_DUMP_ERROR = 9
+JSON_DUMP_ERROR = 11
 JSON_DUMP_ERROR_MSG = "Failed to dump JSON: {}"
-DOWNLOAD_ERROR = 10
+DOWNLOAD_ERROR = 12
