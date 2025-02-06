5354a85a9d34d1103cf5daadcb1591638c745c29
Rasmus Munk <rasmunk@users.noreply.github.com>
2025-02-06 21:37:47 +0100

    Add a checksum validation of the downloaded test image

--- tests/context.py
+++ tests/context.py
@@ -1,13 +1,31 @@
 import os
+import platform
 from gen_vm_image.common.codes import SUCCESS
 from gen_vm_image.image import generate_image
-from configure_vm_image.utils.io import join, makedirs, remove, exists
+from gen_vm_image.utils.net import download_file
+from configure_vm_image.utils.io import join, makedirs, remove, exists, load
 
 
+CPU_ARCHITECTURE = platform.machine()
 TEST_IMAGE_NAME = "test_image"
 TEST_IMAGE_FORMAT = "qcow2"
-INPUT_IMAGE_URL = "https://sid.erda.dk/share_redirect/B0AM7a5lpC/Rocky-9-GenericCloud-Base-9.3-20231113.0.x86_64.qcow2"
-INPUT_IMAGE_CHECKSUM = "7713278c37f29b0341b0a841ca3ec5c3724df86b4d97e7ee4a2a85def9b2e651"
+INPUT_IMAGE_URL = "https://sid.erda.dk/share_redirect/B0AM7a5lpC/Rocky-9-GenericCloud-Base.latest.{}.qcow2".format(
+    CPU_ARCHITECTURE
+)
+INPUT_IMAGE_CHECKSUM_URL = "https://sid.erda.dk/share_redirect/B0AM7a5lpC/Rocky-9-GenericCloud-Base.latest.{}.qcow2.chksum.txt".format(
+    CPU_ARCHITECTURE
+)
+
+
+def extract_checksum_from_image_file(path):
+    """Extract checksum, expects the format 'checksum (of first xxxxx bytes) relative_serverside_path'"""
+    content = load(path)
+    if not content:
+        return False
+    split_content = content.split(" ")
+    checksum = split_content[0]
+    checksum_byte_size = split_content[3]
+    return checksum, checksum_byte_size
 
 
 class AsyncConfigureTestContext:
@@ -29,7 +47,6 @@ class AsyncConfigureTestContext:
             "name": TEST_IMAGE_NAME,
             "size": "3G",
             "input": INPUT_IMAGE_URL,
-            "input_checksum": INPUT_IMAGE_CHECKSUM,
             "output_directory": self.test_tmp_directory,
             "output_format": TEST_IMAGE_FORMAT,
         }
@@ -39,13 +56,25 @@ class AsyncConfigureTestContext:
         )
 
         if not exists(self.image):
+            checksum_file_path = join(
+                self.test_tmp_directory, f"{TEST_IMAGE_NAME}.checksum"
+            )
+            downloaded, response = await download_file(
+                INPUT_IMAGE_CHECKSUM_URL, checksum_file_path
+            )
+            assert downloaded
+            assert exists(checksum_file_path)
+            checksum, checksum_byte_size = extract_checksum_from_image_file(
+                checksum_file_path
+            )
             # Test that the image can be configured
             success, msg = await generate_image(
                 self.image_config["name"],
                 self.image_config["size"],
                 input=self.image_config["input"],
-                input_checksum=self.image_config["input_checksum"],
+                input_checksum=checksum,
                 input_checksum_type="sha256",
+                input_checksum_read_bytes=int(checksum_byte_size),
                 output_directory=self.image_config["output_directory"],
                 output_format=self.image_config["output_format"],
             )
